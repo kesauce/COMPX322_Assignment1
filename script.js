fe5d8780de4a733a7d512338bd2d6855523a9b07
Alexis Manosca <alexismanosca@gmail.com>
2026-03-15 22:32:34 +1300

    clicking on a meal shows the recipe

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-// Grabs the categories from the database as soon as the HTML loads
+/**
+ * Grabs the selected categories from the database
+ */
 function getDatabase() {
     fetch("getCategories.php")
         .then((response) => response.json())
@@ -12,30 +14,86 @@ function getDatabase() {
                 )
                 .join("");
         })
-        .catch((error) => {});
+        .catch((error) => {
+            console.error(`An error has occurred: ${error}`);
+        });
 }
 
-// Grabs the food using the category id
+/**
+ * Grabs the meals in a given category and displays it.
+ * @param {String} category 
+ */
 function getFood(category) {
     const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`;
     fetch(url)
         .then((response) => response.json())
         .then((response) => {
             const foodList = document.getElementById("food-list");
-            foodList.innerHTML = response.meals
-                .map(
-                    (meal) => `
-                    <div class="meal-container">
+
+            // Check if the API returned empty
+            if (response.meals == null) {
+                foodList.innerHTML = `<p id="error">There are no meals in this category.</p>`;
+            } else {
+                // Map through the original response and add the following HTML with every item, ensuring that there is no comma in between each item in the new array
+                // Browser mistakes spaces in meal names as the end of an attribute - need to encode and decode it safely
+                foodList.innerHTML = response.meals
+                    .map(
+                        (meal) => `
+                    <div onClick=getRecipe("${encodeURIComponent(meal.strMeal)}") class="meal-container">
                         <h1>${meal.strMeal}</h1>
                         <img src="${meal.strMealThumb}">
                     </div>
             `,
-                )
-                .join("");
+                    )
+                    .join("");
+            }
         })
-        .catch((error) => {});
+        .catch((error) => {
+            console.error(`An error has occurred: ${error}`);
+        });
+}
+
+/**
+ * Grab the recipe of a given meal and display it.
+ * @param {String} food 
+ */
+function getRecipe(food) {
+    // Decode the food and make the fetch request
+    const decodedFood = decodeURIComponent(food); 
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${decodedFood}`;
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((response) => {
+            const recipe = document.getElementById("recipe");
+            const meal = response.meals[0];
+            let ingredientList = [];
+
+            // Grab all the ingredients if it's not null
+            for (let i = 1; i <= 20; i++){
+                let ingredientKey = `strIngredient${i}`;
+                let measureKey = `strMeasure${i}`;
+
+                if (meal[measureKey] == null || meal[ingredientKey] == null || meal[measureKey] == "" || meal[ingredientKey] == "") continue;
+
+                ingredientList.push(`<li>${meal[measureKey]} ${meal[ingredientKey]}</li>`);
+            };
+
+            // Format the instructions
+            let instructions = `<p>${meal.strInstructions}</p>`;
+
+            recipe.innerHTML = `
+                <h2>Ingredients</h2>
+                <ul>
+                ${ingredientList.join('')}
+                </ul>
+                <h2>Instructions</h2>
+                ${instructions}
+            `;
+        })
+        .catch((error) => {
+            console.error(`An error has occurred: ${error}`);
+        });
 }
 
 getDatabase();
-
-// Add a listener click event to each of the category
